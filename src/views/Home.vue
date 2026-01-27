25422b31d540915e83aab9b967a0cfa525834611
plinioledax-coder <plinio.ledax@gmail.com>
2026-01-26 22:34:21 -0300

    Terceiro upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@ import Footer from '../components/layout/Footer.vue'
 // Seções (Certifique-se que os arquivos estão nesta pasta ou ajuste o caminho)
 import Hero from '../components/sections/Hero.vue'
 import Services from '../components/sections/Services.vue'
-import DevSaaS from '../components/sections/DevSaaS.vue'      // Novo: Substitui o ProjectsGrid
+import DevSaas from '../components/sections/DevSaas.vue'      // Novo: Substitui o ProjectsGrid
 import AppleRepair from '../components/sections/AppleRepair.vue' // Novo: Seção exclusiva
 import Testimonials from '../components/sections/Testimonials.vue'
 import Contact from '../components/sections/Contact.vue'
